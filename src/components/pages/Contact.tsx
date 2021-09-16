d5e89f7e19d9ef35185e0c3247e07c596c7f4060
Sneha <snhthakare@gmail.com>
2021-09-16 17:48:56 +0530

    Adds contact page

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 function Contact() {
-  return <div></div>;
+  return (
+    <div className="bg-common">
+      <div className="events-banner">
+        <h1>HOURS & LOCATION</h1>
+        <p>
+          1906 14th Street NW, Washington, DC 20009 (202) 518-7926
+          info@brescadc.com
+        </p>
+        <p>Dine-in</p>
+        <div>
+          <p>Reservations</p>
+          <p>Wednesday | Thursday | Sunday : 5.30 - 9.30PM</p>
+          <p>Friday & Saturday : 5.00 - 10.00 pm</p>
+          <p>Sunday Lunch : 12.30 - 1.30 PM</p>
+          <p>Closed Monday & Tuesday</p>
+        </div>
+        <div>
+          <h2>GETTING TO BRESCA</h2>
+          <p>
+            Metro Accessible via the Yellow and Green lines using the U
+            St./American Civil War metro station - located at U st between 12th
+            and 13th- 5 minute walk to Bresca- walk west to 14th st, make left,
+            1 block down on west side of street near corner of 14th and T st.
+            Bikeshare- one block away at 2000 14th st. NW (Corner 14th and U in
+            front of Walter Reeves Center) Street Parking- Metered Street
+            Parking is available on 14th Street NW and surrounding side street
+            parking also available Closest recommended parking lot- Reeves
+            Center Parking Garage covered parking located on the corner of U St.
+            and 14th St. NW (Building Address 2000 14th St NW. Washington, DC
+            20009, US) Sun-Thurs $10 Fri and Sat $15 // Credit accepted MC/Visa,
+            Debit card, Bills, Amex, Coins
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
